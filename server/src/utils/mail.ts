cfd7b135c7c388eca2757928d9a7020d8c61ae2f
Pablo Sagastizabal <sgszpablo@gmail.com>
2023-06-07 17:41:03 -0300

    Update mail setting

--- server/src/utils/mail.ts
+++ server/src/utils/mail.ts
@@ -3,6 +3,9 @@ import * as nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: config.mailUser,
     pass: config.mailPass,
